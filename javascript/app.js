1517140289cea201026cc667bab7581ac2d5d6b6
Juan Tuñon <primerproyecto@gmail.com>
2022-10-07 14:07:00 +0200

    Ejercicio3#JsLoops

--- javascript/app.js
+++ javascript/app.js
@@ -401,3 +401,47 @@ function repeatCounter(param) {
   console.log(count);
 }
 var cuantosDuplicados = repeatCounter(counterWords);
+
+
+
+// JS LOOPS. EJERCICIOS 3
+
+//Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .includes de javascript.
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+
+var tieneCamiseta = products.filter((palabra) => palabra.includes('Camiseta') );
+console.log(tieneCamiseta);
+
+
+/* **Iteración #2: Condicionales avanzados**
+
+Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***. */
+
+const alumns = [
+  {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+  {name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+  {name: 'Juan Miranda', T1: false, T2: true, T3: true},
+  {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+  {name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+var mappedAlumns = alumns.map((alumn)=> {
+  var trimestresAprobados = 0;
+  if(alumn.T1 === true){
+    ++trimestresAprobados;
+  }
+  if(alumn.T2 === true){
+    ++trimestresAprobados;
+  }
+  if(alumn.T3 === true){
+    ++trimestresAprobados;
+  }
+
+  return {
+    'name' : alumn.name,
+    'isApproved': trimestresAprobados >= 2 ? true : false
+  }
+})
+console.log(mappedAlumns)
+
+
