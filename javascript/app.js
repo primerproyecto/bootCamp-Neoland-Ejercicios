9bbe57b460cc9e83c79a50bff971eebc52f083a6
Juan Tuñon <primerproyecto@gmail.com>
2022-10-09 14:06:26 +0200

    neoland-es6

--- javascript/app.js
+++ javascript/app.js
@@ -602,3 +602,23 @@ console.log('número aleatorio ' + rollDice(6));
 
 Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante. */
 
+/* JS ES6 */
+
+/* Crea una arrow function que tenga dos parametros a y b y 
+que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+por consola la suma de los dos parametros. */
+const primeraFuncion = (a = 10, b = 5) => {
+  console.log( a + b);
+  return a + b
+}
+/* 1.1 Ejecuta esta función sin pasar ningún parametro */
+const resultado = primeraFuncion();
+console.log(resultado);
+
+/* 1.2 Ejecuta esta función pasando un solo parametro */
+const resultadoSoloUnParam = primeraFuncion(3);
+console.log(resultadoSoloUnParam)
+
+/* 1.3 Ejecuta esta función pasando dos parametros */
+const resultadoDosParam = primeraFuncion(3,4);
+console.log(resultadoDosParam);
